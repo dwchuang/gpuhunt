251ef3321743da4cabbfebad32b0420597dbe216
dwchuang <chuang44@purdue.edu>
2024-11-02 14:15:48 -0400

    Add files via upload

--- src/gpuhunt/_internal/catalog.py
+++ src/gpuhunt/_internal/catalog.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov  2 12:33:09 2024
-
-@author: dwchuang_mbp2
-"""
 import csv
 import dataclasses
 import heapq
@@ -25,7 +18,7 @@ from gpuhunt.providers import AbstractProvider
 logger = logging.getLogger(__name__)
 version_url = "https://dstack-gpu-pricing.s3.eu-west-1.amazonaws.com/v1/version"
 catalog_url = "https://dstack-gpu-pricing.s3.eu-west-1.amazonaws.com/v1/{version}/catalog.zip"
-OFFLINE_PROVIDERS = ["aws", "azure", "crusoe", "datacrunch", "gcp", "lambdalabs", "oci", "runpod"]  # Added crusoe
+OFFLINE_PROVIDERS = ["aws", "azure", "datacrunch", "gcp", "lambdalabs", "oci", "runpod"]
 ONLINE_PROVIDERS = ["cudo", "tensordock", "vastai"]
 RELOAD_INTERVAL = 15 * 60  # 15 minutes
 
